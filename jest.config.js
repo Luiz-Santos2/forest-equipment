c86fc9af31dc742ed67a84ded4693661809fd4a7
Luiz Clovis Dos Santos Matias <luizclovis2014@gmail.com>
2024-09-19 23:42:16 -0300

    Ajustes no projeto

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,19 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest',
-      '^.+\\.(js|jsx)$': 'babel-jest'
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!react-leaflet)', // Permitir que o react-leaflet seja transformado
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json', // Ajuste se necessário para o seu tsconfig
     },
-    transformIgnorePatterns: [
-      '/node_modules/(?!react-leaflet|leaflet|some-other-modules)'
-    ],
-    moduleNameMapper: {
-      '\\.(css|less)$': 'identity-obj-proxy'
-    },
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-    globals: {
-      'ts-jest': {
-        tsconfig: 'tsconfig.json'
-      }
-    }
-  };
-  
+  },
+};
